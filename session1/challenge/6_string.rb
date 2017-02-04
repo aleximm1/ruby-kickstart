2844366e4ac795cd8d716e254f047c6240d579a1
Alex Imm <alexanderimm@hotmail.co.uk>
2017-02-04 14:01:16 +0000

    S1 Finished

--- session1/challenge/6_string.rb
+++ session1/challenge/6_string.rb
@@ -1,11 +1,34 @@
 # You'll get a string and a boolean.
 # When the boolean is true, return a new string containing all the odd characters.
 # When the boolean is false, return a new string containing all the even characters.
-# 
+#
 # If you have no idea where to begin, remember to check out the cheatsheets for string and logic/control
-# 
-# odds_and_evens("abcdefg",true)    # => "bdf"
-# odds_and_evens("abcdefg",false)   # => "aceg"
+#
+#odds_and_evens("abcdefg",true)    # => "bdf"
+#odds_and_evens("abcdefg",false)   # => "aceg"
 
 def odds_and_evens(string, return_odds)
+  oddOrEven = 0
+  new_string = ""
+  string.each_char do |c|
+    if return_odds == true
+      if oddOrEven == 1
+        new_string << c
+        oddOrEven = 0
+      else
+        oddOrEven = 1
+      end
+    elsif return_odds == false
+      if oddOrEven == 1
+        oddOrEven = 0
+      else
+        new_string << c
+        oddOrEven = 1
+      end
+    end
+  end
+  puts new_string
 end
+
+odds_and_evens("abcdefg",true)    # => "bdf"
+odds_and_evens("abcdefg",false)   # => "aceg"
--- session1/challenge/7_string.rb
+++ session1/challenge/7_string.rb
@@ -1,8 +1,24 @@
 # given a string, return the character after every letter "r"
-# 
+#
 # pirates_say_arrrrrrrrr("are you really learning Ruby?") # => "eenu"
 # pirates_say_arrrrrrrrr("Katy Perry is on the radio!")   # => "rya"
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
+  r = false
+  output_string = ""
+  string.each_char do |x|
+    if r == true
+      output_string << x
+      r = false
+    end
+    if x == "r" || x == "R"
+      r = true
+    end
+  end
+  puts output_string
 end
+
+pirates_say_arrrrrrrrr("are you really learning Ruby?") # => "eenu"
+pirates_say_arrrrrrrrr("Katy Perry is on the radio!")   # => "rya"
+pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
